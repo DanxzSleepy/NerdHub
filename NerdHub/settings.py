3f30cec4fb689411b22f29f15ef7941c74221c94
DanxzSleepy <wolfdangalaxy2@gmail.com>
2025-11-12 17:48:08 -0300

    testa allowed hosts com wildcard

--- NerdHub/settings.py
+++ NerdHub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['nerdhub.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
